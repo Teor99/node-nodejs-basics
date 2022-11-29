53444b35b343e412ed635310f3672925c0d81e04
Dmitriy Smirnov <mygmbox26@gmail.com>
2022-11-29 18:47:09 +0500

    feat: create file task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import url from 'node:url';
+
 const create = async () => {
-    // Write your code here 
+    const dirname = path.dirname(url.fileURLToPath(import.meta.url));
+    const filePath = path.join(dirname, './files/fresh.txt');
+
+    try {
+        await fs.access(filePath, fs.constants.F_OK);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code == 'ENOENT') {
+            await fs.writeFile(filePath, 'I am fresh and young', 'utf8');
+        } else {
+            throw error;
+        }
+    }
 };
 
 await create();
