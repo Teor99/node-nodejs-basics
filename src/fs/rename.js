a2918b07c21b0d9edf35ec4c1f54fc789bc28ffc
Dmitriy Smirnov <mygmbox26@gmail.com>
2022-11-30 13:50:52 +0500

    feat: solution for task - rename file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import path from 'node:path';
+import url from 'node:url';
+import fsPromise from 'node:fs/promises';
+import fs from 'node:fs';
+
 const rename = async () => {
-    // Write your code here 
+    const currentDir = path.dirname(url.fileURLToPath(import.meta.url));
+    const sourceFilePath = path.join(currentDir, './files/wrongFilename.txt');
+    const destFilePath = path.join(currentDir, './files/properFilename.md');
+    try {
+        if (fs.existsSync(sourceFilePath) && !fs.existsSync(destFilePath)) {
+            fsPromise.rename(sourceFilePath, destFilePath);
+        } else {
+            throw new Error();
+        }
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
