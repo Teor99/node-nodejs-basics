a35bb1ac214fdb1de2c8c3d68cd40b7f7d95d17b
Dmitriy Smirnov <mygmbox26@gmail.com>
2022-12-01 20:18:45 +0500

    feat: solution for task - command line arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = [];
+    for (let i = 2; i < process.argv.length; i += 2) {
+        const argName = process.argv[i];
+        const argValue = process.argv[i + 1];
+        args.push(`${argName}=${argValue}`);
+    }
+    console.log(args.join(', '));
 };
 
 parseArgs();
