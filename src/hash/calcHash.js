6e66e5119575a02243832827178b5aa0e79bf4cf
Dmitriy Smirnov <mygmbox26@gmail.com>
2022-12-02 00:27:59 +0500

    feat: solution for task - calc file hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import path from 'node:path';
+import url from 'node:url';
+import crypto from 'node:crypto';
+import fs from 'node:fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const currentDir = path.dirname(url.fileURLToPath(import.meta.url));
+    const targetFile = path.join(currentDir, './files/fileToCalculateHashFor.txt');
+    const fileData = await fs.readFile(targetFile);
+    const hashSum = crypto.createHash('sha256');
+    hashSum.update(fileData);
+    console.log(hashSum.digest('hex'));
 };
 
 await calculateHash();
