0a9a4225ab854cf9af86ad34b2a847ba5781b40d
Dmitriy Smirnov <mygmbox26@gmail.com>
2022-12-02 22:05:31 +0500

    feat: solution for task - write to stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import url from 'node:url';
+
 const write = async () => {
-    // Write your code here 
+    const currentDir = path.dirname(url.fileURLToPath(import.meta.url));
+    const targetFile = path.join(currentDir, './files/fileToWrite.txt');
+    const fileDescriptor = await fs.open(targetFile, 'w');
+    const writeStream = fileDescriptor.createWriteStream();
+    process.stdin.pipe(writeStream);
 };
 
 await write();
